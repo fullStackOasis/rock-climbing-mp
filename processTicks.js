abda092541f56b26b95aad475bbdc811a38fed46
fullStackOasis <fullstackdev@fullstackoasis.com>
2025-06-26 10:19:44 -0400

    Create simple library to read csv file, use that to get data into script.

--- processTicks.js
+++ processTicks.js
@@ -1,2 +1,12 @@
+"use strict"
+const {getCSVContentAsArray} = require(`./lib/readCSV.js`);
 
-console.log(`Hello world!`);
+const readTicks = async (filename) => {
+    console.log(`Hello world!`);
+    const arr = await getCSVContentAsArray(filename);
+    console.log(`Printing csv results:`);
+    console.log(JSON.stringify(arr));
+}
+
+const blah = readTicks(`sample.csv`);
+console.log(`done`);
